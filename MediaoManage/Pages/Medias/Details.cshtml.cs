0cd79376d76e5f6e1c7fbd4c51cd15e062457ff3
cleverman <first1124.dev.master@gmail.com>
2023-07-27 10:18:30 -0700

    update azure connection string name

--- MediaoManage/Pages/Medias/Details.cshtml.cs
+++ MediaoManage/Pages/Medias/Details.cshtml.cs
@@ -16,8 +16,8 @@ namespace MediaoManage.Pages.Medias
         private readonly MediaoManage.Data.MediaoManageContext _context;
         public BlobContainerClient blobContainer { get; set; }
         public string blobContainerName = "movie";
-        public string StorageConnectionString = "UseDevelopmentStorage=true";
-        //const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=youtubedev;AccountKey=YVHIVv+u2BzVxYPagFotweQNV+9uotA41Oswq+DHX24ALGgPd7ugq2bVu30tRJkLvN3hCvV7PQM3+AStHVNC/Q==;EndpointSuffix=core.windows.net";
+        //public string StorageConnectionString = "UseDevelopmentStorage=true";
+        const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=youtubedev;AccountKey=YVHIVv+u2BzVxYPagFotweQNV+9uotA41Oswq+DHX24ALGgPd7ugq2bVu30tRJkLvN3hCvV7PQM3+AStHVNC/Q==;EndpointSuffix=core.windows.net";
         public DetailsModel(MediaoManage.Data.MediaoManageContext context)
         {
             _context = context;
